d91ba72cf2be7eea25dc696a9c3551f912f17a7f
Brentsa <seanbrent5@gmail.com>
2022-06-20 10:22:48 -0400

    updated password hashing in account create api route

--- pages/api/account/create.ts
+++ pages/api/account/create.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Account, Prisma } from '@prisma/client'
+import { Account } from '@prisma/client'
 import { prisma } from '../../../db/prisma';
 import { getPrismaClientError } from '../../../utils/helpers';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
 
 type Data = Omit<Account, 'password'> | {error: string};
 
@@ -12,31 +12,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     const {username, email, password} = req.body;
 
-    try{
-        //try creating a user with the supplied credentials
-        const newAccount: Omit<Account, 'password'> = await prisma.account.create({
-            data: {
-                firstName: req.body?.firstName,
-                lastName: req.body?.lastName,
-                username,
-                email,
-                //hash the supplied password in the database
-                password: await bcrypt.hash(password, 10)
-            },
-            select: {
-                id: true,
-                firstName: true,
-                lastName: true,
-                username: true,
-                email: true,
-                createdAt: true
-            }
-        })
+    //hash the supplied password and then run the prisma create operation if no error
+    bcrypt.hash(password, 10, async function(err, encrypted){
+        if(err) return res.status(400).json({error: 'Account not created.'});
 
-        //If there are no errors, return the account details
-        return res.status(200).json(newAccount);
-    }
-    catch(e){
-        return res.status(400).json({error: 'Account not created. ' + getPrismaClientError(<PrismaClientKnownRequestError>e)});
-    }
+        try{
+            //try creating a user with the supplied credentials
+            const newAccount: Omit<Account, 'password'> = await prisma.account.create({
+                data: {
+                    firstName: req.body?.firstName,
+                    lastName: req.body?.lastName,
+                    username,
+                    email,
+                    password: encrypted
+                },
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    username: true,
+                    email: true,
+                    createdAt: true
+                }
+            });
+
+            //If there are no errors, return the account details
+            return res.status(200).json(newAccount);
+        } 
+        catch(e){
+            return res.status(400).json({error: 'Account not created. ' + getPrismaClientError(<PrismaClientKnownRequestError>e)});
+        }
+    }); 
 }
