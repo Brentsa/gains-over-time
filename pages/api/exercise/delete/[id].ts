6897e59612f755521e77b4d0a7a5f60b9eb6c6c2
Brentsa <seanbrent5@gmail.com>
2022-06-27 08:30:21 -0400

    Delete exercise api route created and get exercises returns account info

--- pages/api/exercise/delete/[id].ts
+++ pages/api/exercise/delete/[id].ts
@@ -1,23 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Muscle } from '@prisma/client'
+import { Exercise } from '@prisma/client'
 import { prisma } from '../../../../db/prisma';
 import { getPrismaClientError } from '../../../../utils/helpers';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
-type Data = Muscle | {error: string}
+type Data = Exercise | {error: string}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     if(req.method !== 'DELETE') return res.status(405).json({error: 'Incorrect request method.'})
     
     try {
-        //delete the muscle associated with the supplied ID from the request query
-        const deletedMuscle = await prisma.muscle.delete({
+        //delete the exercise associated with the queried id
+        const deletedExercise = await prisma.exercise.delete({
             where: {
                 id: parseInt(<string>req.query.id)
             }
         })
 
-        return res.status(200).json(deletedMuscle);
+        return res.status(200).json(deletedExercise);
     } 
     catch (e) {
         return res.status(400).json({error: 'Muscle not deleted. ' + getPrismaClientError(<PrismaClientKnownRequestError>e)});
--- pages/api/exercises.ts
+++ pages/api/exercises.ts
@@ -7,10 +7,23 @@ type Data = Exercise[] | {error: string};
 export default async function handler(req:NextApiRequest, res:NextApiResponse<Data>){
     if(req.method !== 'GET') return res.status(405).json({error: 'Incorrect request method.'});
 
-    //find all exercises and include their related muscles
+    //find all exercises and include their related muscles and account holder
     const allExercises = await prisma.exercise.findMany({
         include: {
-            muscles: true
+            account: {
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    username: true
+                }
+            },
+            muscles: {
+                select: {
+                    id: true,
+                    name: true
+                }
+            }
         }
     });
 
