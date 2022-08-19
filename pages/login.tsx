7f3101eaf7506e3454dc16958ec7fc3936731239
Brentsa <seanbrent5@gmail.com>
2022-08-19 08:35:55 -0400

    added user session check to login and register pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,7 @@
+import { withIronSessionSsr } from "iron-session/next";
 import Head from "next/head"
 import LoginForm from "../components/LoginForm"
+import { ironOptions } from "../utils/iron-session-config";
 
 export default function Login(){
   
@@ -13,5 +15,21 @@ export default function Login(){
       <LoginForm/>
     </div>
   )
-  
 }
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user;
+
+    //if there is no user in session, return no props to the page
+    if (!user)  return { props: {} };
+
+    //if there is a user session, redirect them to homepage
+    return { 
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    };
+  }, ironOptions
+);
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,15 +1,34 @@
+import { withIronSessionSsr } from "iron-session/next";
 import Head from "next/head"
 import RegisterForm from "../components/RegisterForm"
+import { ironOptions } from "../utils/iron-session-config";
 
 export default function Register(){
-    return (
-      <div>
-        <Head>
-          <title>Register - Gains Over Time</title>
-          <meta name="description" content="Track your workout progess over time." />
-          <link rel="icon" href="/favicon.ico"/>
-        </Head>
-        <RegisterForm/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Head>
+        <title>Register - Gains Over Time</title>
+        <meta name="description" content="Track your workout progess over time." />
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user;
+
+    //if there is no user in session, return no props to the page
+    if (!user)  return { props: {} };
+
+    //if there is a user session, redirect them to homepage
+    return { 
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    };
+  }, ironOptions
+);
