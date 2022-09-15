a5d22c82bc5d7d2d397faadb499fe7df273c13a6
Brentsa <seanbrent5@gmail.com>
2022-09-14 22:16:26 -0400

    Revised the exercise update route to accomodate exercise templates

--- pages/api/exercise/update/[id].ts
+++ pages/api/exercise/update/[id].ts
@@ -5,19 +5,19 @@ import { getPrismaClientError } from '../../../../utils/helpers';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 type Data = Exercise | {error: string}
-type Muscle = {id: number};
+type Sets = {id: number};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>){
     if(req.method !== 'PUT') return res.status(405).json({error: 'Incorrect request method.'});
 
     const { id } = req.query;
 
-    //take the name from the request body if present
-    const name: string | undefined = req.body?.name;
-
     //if there is a muscles array in the request, map the array of ids to a muscle id array for prisma [{id: 1},{id: 2},...]
-    const muscleIds: number[] | undefined = req.body?.muscles;
-    const muscles:Muscle[] | undefined = muscleIds ? muscleIds.map(muscleId => <Muscle>{id: muscleId}) : undefined;
+    // const muscleIds: number[] | undefined = req.body?.muscles;
+    // const muscles:Muscle[] | undefined = muscleIds ? muscleIds.map(muscleId => <Muscle>{id: muscleId}) : undefined;
+
+    const setIds: number[] | undefined = req.body.sets;
+    const sets: Sets[] | undefined = setIds ? setIds.map(setId => <Sets>{id: setId}): undefined;
 
     try{
         const updatedExercise = await prisma.exercise.update({
@@ -25,10 +25,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                 id: parseInt(<string>id)
             },
             data:{
-                name: name, 
-                muscles: {
-                    set: muscles
+                sets: {
+                    set: sets
                 }
+                // muscles: {
+                //     set: muscles
+                // }
             },
             include: {
                 account: {
@@ -37,12 +39,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                         firstName: true,
                         lastName: true,
                         username: true
-                    }
-                },
-                muscles: {
-                    select: {
-                        id: true,
-                        name: true
                     }
                 }
             }
