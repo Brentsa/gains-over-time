6d06a8edfe57a6008c7ddf35c035f175830d4008
Brentsa <seanbrent5@gmail.com>
2022-10-08 12:02:27 -0400

    created exercise template get route based on queried account id

--- pages/api/exercise-templates/[accountId].ts
+++ pages/api/exercise-templates/[accountId].ts
@@ -1,14 +1,20 @@
 import { ExerciseTemplate } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../db/prisma';
+import { prisma } from '../../../db/prisma';
 
 type Data = ExerciseTemplate[] | {error: string};
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse<Data>){
     if(req.method !== 'GET') return res.status(405).json({error: 'Incorrect request method.'});
 
+    //take the account id from the request and store it as a number
+    const userId: number = parseInt(req.query.accountId as string);
+
     //find all exercises and include their related muscles and account holder
     const allExerciseTemplates = await prisma.exerciseTemplate.findMany({
+        where: {
+            accountId: userId
+        },
         include: {
             account: {
                 select: {
---
+++ pages/api/exercise-templates/index.ts
@@ -0,0 +1,31 @@
+import { ExerciseTemplate } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../db/prisma';
+
+type Data = ExerciseTemplate[] | {error: string};
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse<Data>){
+    if(req.method !== 'GET') return res.status(405).json({error: 'Incorrect request method.'});
+
+    //find all exercises and include their related muscles and account holder
+    const allExerciseTemplates = await prisma.exerciseTemplate.findMany({
+        include: {
+            account: {
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    username: true
+                }
+            },
+            muscles: {
+                select: {
+                    id: true, 
+                    name: true
+                }
+            }
+        }
+    });
+
+    return res.status(200).json(allExerciseTemplates);
+}
