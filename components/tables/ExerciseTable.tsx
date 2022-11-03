36d39b9079f378e23fb719bc462d683d59dee344
Brentsa <seanbrent5@gmail.com>
2022-11-03 18:16:59 -0400

    Modal component close function added as well as proper styling

--- components/tables/ExerciseTable.tsx
+++ components/tables/ExerciseTable.tsx
@@ -4,6 +4,7 @@ import fetcher from "../../utils/swrFetcher";
 import ExerciseTableRow from "./ExerciseTableRow";
 import { ExerciseTemplate, Set } from "@prisma/client";
 import Modal from "../utilites/Modal";
+import { MouseEvent, useState } from "react";
 
 export interface ExerciseFromSWR{
     id: number,
@@ -19,13 +20,28 @@ export default function ExerciseTable({user}: Props){
     //fetch all of the user's exercises using their ID
     const {data, error} = useSWR<ExerciseFromSWR[]>(`api/exercises/${user?.id}`, fetcher);
 
-    console.log(data);
+    //state to control the modal open and close state
+    const [modalOpen, setModalOpen] = useState<boolean>(true);
+
+    //Set modal open state to true
+    function openModal(){
+        return setModalOpen(true);
+    }
+
+    //Set the modal open state to false
+    function closeModal(event: MouseEvent<HTMLButtonElement | HTMLDivElement>){
+        event.preventDefault();
+        event.stopPropagation();
+        return setModalOpen(false);
+    }
 
     if(!data || error) return <div>Exercises could not load.</div>
 
     return (
         <div>
-            <Modal/>
+            <Modal open={modalOpen} closeModal={closeModal}>
+                <div>Hello There!!</div>
+            </Modal>
             <div className='w-full h-0.5 bg-gradient-to-r from-rose-400 via-violet-400 to-rose-400'/>
             <ul>
                 {data.map((exercise, i) => <ExerciseTableRow key={i} exercise={exercise}/>)}
--- components/utilites/Modal.tsx
+++ components/utilites/Modal.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { MouseEvent, ReactNode } from "react";
 import Paper from "./Paper";
 
-export default function Modal(){
+interface Props {
+    open: boolean,
+    closeModal: (event: MouseEvent<HTMLButtonElement | HTMLDivElement>) => void,
+    children: ReactNode
+}
 
-    const [open, setOpen] = useState<boolean>(false);
+export default function Modal({open, closeModal, children}: Props){
 
-    if(!open) return; 
 
-    return (
-        <div className="z-40 fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-40 flex items-center justify-center">
-            <Paper className="w-1/3 h-1/2">
-                What we doing here bud
+    return open ? (
+        <div className="z-40 fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex items-center justify-center">
+            <Paper className="w-1/3 h-1/2 relative shadow-xl shadow-gray-900/50">
+                <button className="absolute top-2 right-3 text-rose-500" onClick={closeModal}>
+                    <FontAwesomeIcon size="2x" icon={faXmark} />
+                </button>
+                <section className="w-full h-full flex items-center justify-center">
+                    {children}
+                </section>
             </Paper>
         </div>
-    );
+    ): <></>;
 }
