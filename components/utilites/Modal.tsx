1cc16fec53f527d794fb27d59761605b343b3cef
Brentsa <seanbrent5@gmail.com>
2022-12-26 14:08:16 -0500

    removed paper from Modal

--- components/utilites/Modal.tsx
+++ components/utilites/Modal.tsx
@@ -1,7 +1,6 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ReactNode, useEffect } from "react";
-import Paper from "../utilites/Paper";
 
 interface Props {
     open: boolean,
@@ -19,14 +18,14 @@ export default function Modal({open, closeModal, children}: Props){
 
     return open ? (
         <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-gray-500 bg-opacity-50 flex items-center justify-center">
-            <Paper className="w-10/12 py-8 sm:py-10 lg:py-12 sm:w-5/12 lg:w-fit lg:max-w-2xl relative shadow-xl shadow-gray-900/50 pointer-events-auto">
+            <div className="bg-white p-2 sm:p-4 lg:p-6 sm:rounded w-10/12 py-8 sm:py-10 lg:py-12 sm:w-5/12 lg:w-fit lg:max-w-2xl relative shadow-xl shadow-gray-900/50 pointer-events-auto">
                 <button className="absolute top-2 right-3 text-rose-500" onClick={closeModal}>
                     <FontAwesomeIcon size="2x" icon={faXmark} />
                 </button>
                 <section className="w-full h-full flex items-center justify-center">
                     {children}
                 </section>
-            </Paper>
+            </div>
         </div>
     ): <></>;
 }
