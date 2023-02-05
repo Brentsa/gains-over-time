243d48c2ea378242b9ccaea28f566e1680020f4c
Brentsa <seanbrent5@gmail.com>
2023-02-05 13:28:46 -0500

    Fixed manifest page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,19 +17,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="description" content="Track your workout progess over time." />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          href="/public/icons/Logo_320.png"
-          rel="icon"
-          type="image/png"
-          sizes="320x320"
-        />
-        <link
-          href="/public/icons/Logo_32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" sizes="320x320" href='/public/icons/Logo_320.png' />
         <link rel='manifest' href='/manifest.json' />
       </Head>
       <Component {...pageProps} />
