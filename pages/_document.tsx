972b7b286c61f898bb9a05ddb84b3e492145ac5e
Brentsa <seanbrent5@gmail.com>
2023-02-19 11:53:23 -0500

    fixed background to display correctly on iOS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="h-screen" style={{
-        background: 'linear-gradient(to left top, #fca5a5 0%, 10%, #e5e7eb 50%, 80%, #c4b5fd 90%)', 
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed'
-      }}>
+    <Html>
         <Head/>
         <body className="h-full">
             <Main />
