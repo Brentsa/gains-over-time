eace59f240a04906f9a6e55a20371ee18e63c8f6
Brentsa <seanbrent5@gmail.com>
2023-02-25 13:02:14 -0500

    Added a blur and focus event to search input for mobile use

--- components/MobileSearchBar.tsx
+++ components/MobileSearchBar.tsx
@@ -6,6 +6,7 @@ import SearchBar from "./SearchBar";
 export default function MobileSearchBar(){
 
     const [open, setOpen] = useState<boolean>(false);
+    const [focus, setFocus] = useState<boolean>(false);
 
     //toggle the search bar open
     function toggleOpen(){
@@ -21,15 +22,15 @@ export default function MobileSearchBar(){
     })
 
     return (
-        <div className="h-screen w-screen fixed top-0 left-0">
-            <div className="right-0 bottom-96 bot absolute p-2 transition-all z-50">
+        <div className="h-screen w-screen fixed top-0 left-0 pointer-events-none">
+            <div className={`right-0 ${focus ? 'bottom-96' : 'bottom-4'} bot absolute p-2 transition-all z-40 pointer-events-auto`}>
                 <div 
                     className={`bg-rose-500 p-1 rounded-full shadow-lg transition-all duration-150 flex justify-between items-center overflow-hidden`}
                     style={{width: open ? screenWidth.current-16 : 56}}
                 >
                     {open &&
                         <div className="w-4/5 ml-1">
-                            <SearchBar rounded/>
+                            <SearchBar rounded setFocus={setFocus}/>
                         </div>
                     }
                     <IconButton 
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { searchContext } from "./MainPageContent";
 
 interface Props {
     rounded? : boolean
+    setFocus? : Dispatch<SetStateAction<boolean>>
 }
 
 export default function SearchBar(props: Props){
@@ -13,6 +14,16 @@ export default function SearchBar(props: Props){
 
     function handleChange(event: ChangeEvent<HTMLInputElement>){
         setSearch(event.target.value);
+    }
+
+    //if there is a set focus props, provide a focus function to the search input
+    function handleFocus(){
+        return props.setFocus ? props.setFocus(true) : undefined
+    }
+
+    //if there is a set focus props, provide a blur function to the search input
+    function handleBlur(){
+        return props.setFocus ? props.setFocus(false) : undefined
     }
 
     //blur the search bar if the form is submitted
@@ -37,6 +48,8 @@ export default function SearchBar(props: Props){
                     onChange={handleChange}
                     className={`appearance-none shadow-inner ${props?.rounded ? 'rounded-full' : 'rounded'} relative block w-full px-3 py-2 sm:px-2 sm:py-1 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-1 focus:ring-violet-400 focus:border-violet-400 focus:z-10`}
                     placeholder="Search Exercise History"
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
                 />
             </form>
             <div className={`absolute top-1/2 -translate-y-1/2 ${props?.rounded ? 'right-3' : 'right-2'} z-20 flex items-center text-xl`}>
