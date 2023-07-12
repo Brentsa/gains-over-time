9d269337735ae252735fde778f5fc13510960e68
Brentsa <seanbrent5@gmail.com>
2023-07-12 08:21:26 -0400

    Stopped scrolling form occuring when the user starts a left or right swipe

--- components/tables/WeightTableRow.tsx
+++ components/tables/WeightTableRow.tsx
@@ -4,9 +4,10 @@ import { formatDateShortMonth } from "../../utils/helpers"
 import { SwipeEventData, useSwipeable } from "react-swipeable"
 import { useContext, useEffect, useRef, useState } from "react"
 import IconButton from "../buttons/IconButton"
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons"
+import { faCaretLeft, faTrashCan } from "@fortawesome/free-solid-svg-icons"
 import { feedbackContext } from "../MainPageContainer"
 import { mutate } from "swr"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 interface Props{
     weightRecord: Weight
@@ -19,23 +20,36 @@ export default function WeightTableRow({weightRecord}:Props){
     //state to determine if the row is swiped open or not
     const [swipedOpen, setSwipedOpen] = useState(false);
 
+    const [stopScroll, setStopScroll] = useState(false);
+
     //handle user swiping based on direction to open or close the row
     function handleSwipe(eventData: SwipeEventData){
-        //console.log("User Swiped!", eventData);
+        console.log("User Swiped!", eventData);
         switch (eventData.dir) {
             case 'Left':
                 setSwipedOpen(true);
+                setStopScroll(false);
                 break;
         
             case 'Right':
                 setSwipedOpen(false);
+                setStopScroll(false);
                 break;
         }
     };
 
+    //stop scrolling function at the start of a left or right swipe
+    function startSwipe(eventData: SwipeEventData){
+        //console.log(eventData);
+        if(eventData.dir === 'Left' || eventData.dir === 'Right'){
+            setStopScroll(true);
+        }
+    }
+
     //define swipe handlers for the component
     const handlers = useSwipeable({
         onSwiped: handleSwipe,
+        onSwipeStart: startSwipe,
         trackMouse: true
     });
 
@@ -56,7 +70,10 @@ export default function WeightTableRow({weightRecord}:Props){
 
     return (
         <div className="w-full md:w-1/2 h-12 relative select-none" {...handlers}>
-            <Paper className={`absolute transition-all ${swipedOpen ? '-left-14' : 'left-0'} flex justify-between items-center px-4 w-full h-full rounded z-10`}>
+            <div className="absolute -left-2 z-0">
+                <FontAwesomeIcon icon={faCaretLeft} size="3x" className="text-violet-300"/>
+            </div>
+            <Paper className={`absolute transition-all ${swipedOpen ? '-left-14' : 'left-0'} ${stopScroll ? 'touch-none bg-gray-100' : 'touch-auto'} flex justify-between items-center px-4 w-full h-full rounded z-10`}>
                 <h2 className="text-2xl">{weightRecord.weight} <span className="text-sm">{weightRecord.massUnit}</span></h2>
                 <p className="text-sm">{formatDateShortMonth(weightRecord.createdAt)}</p>
             </Paper>
